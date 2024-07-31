a6ed31a3d6457146e85c8e73393632ee128e9bab
Daniel <102395937+maddsua@users.noreply.github.com>
2024-07-31 11:22:33 +0000

    deploy to pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
+	base: '/cranecalc/',
 	plugins: [svelte({
 		preprocess: [
 			vitePreprocess()
