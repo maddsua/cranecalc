ec413e1ccabd59a08336143afb75604851d5dbf2
maddsua <maddsuadev@proton.me>
2024-08-01 19:47:45 +0300

    moved to cf

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
-	base: '/cranecalc/',
 	plugins: [svelte({
 		preprocess: [
 			vitePreprocess()
